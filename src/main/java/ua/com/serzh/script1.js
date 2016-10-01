9c2130cbb4b518536870890f34666599f66577c4
Just-Fun <Just-Fun@users.noreply.github.com>
2016-10-01 20:23:05 +0300

    start project

---
+++ src/main/java/ua/com/serzh/script1.js
@@ -0,0 +1,17 @@
+"use strict"
+
+
+
+alert( 1 + '2' );
+/*var isAdmin = prompt("Who are you?", "admin");
+if (isAdmin === "admin") {
+    alert("!");
+} else if (isAdmin == null) {
+    alert("canceled...")
+} else {
+    alert("...")
+}
+
+alert( isAdmin );*/
+
+//var free = prompt("Are you free?", "yes");
